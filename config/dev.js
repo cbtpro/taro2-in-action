4a6ca7f0c28d22c9d18cf90d78799bc46fd4b9cd
陈碧滔 <cbtpro@163.com>
2022-06-24 19:23:59 +0800

    feat: 增加h5的ip访问；修改端口；代理配置

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,26 @@ module.exports = {
   },
   defineConstants: {},
   mini: {},
-  h5: {}
+  h5: {
+    // https://taro-docs.jd.com/taro/docs/2.x/config-detail#h5devserver
+    devServer: {
+      host: '0.0.0.0',
+      port: 10088,
+      proxy: {
+        '/server1/api/**/*': {
+          target: 'localhost:8080',
+          pathRewrite(path) {
+            return path.replace('/api', '/');
+          },
+          logLevel: 'debug',
+          changeOrigin: true,
+        },
+        '/server2/**/*': {
+          target: 'localhost:8081',
+          logLevel: 'debug',
+          changeOrigin: true,
+        },
+      },
+    },
+  }
 }
